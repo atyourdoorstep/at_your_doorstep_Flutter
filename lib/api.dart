00023b0bcc91cdac06204d1dad79685f7b71036a
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-08-02 19:53:49 +0500

    Initial commit

---
+++ lib/api.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+class CallApi{
+  final String _url = 'http://10.0.2.2:8000/';
+  CallApi()
+   {
+  }
+  postData(data, apiUrl) async {
+    Uri fullUrl = Uri.parse(_url + apiUrl + await _getToken());
+
+    return await http.post(
+        fullUrl,
+        body: jsonEncode(data),
+        headers: _setHeaders()
+    );
+
+  }
+  getData(apiUrl) async {
+    Uri fullUrl = Uri.parse(_url + apiUrl + await _getToken());
+    var resp=await http.get(
+        fullUrl,
+        headers: _setHeaders()
+    );
+    if (resp.statusCode == 200) {
+      String data = resp.body;
+      return jsonDecode(data);
+    } else {
+      print(resp.statusCode);
+    }
+
+    return JsonCodec();
+  }
+
+
+
+
+  _setHeaders() => {
+    'Content-type' : 'application/json',
+    'Accept' : 'application/json',
+  };
+
+  _getToken() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var token = localStorage.getString('token');
+    return '?token=$token';
+  }
+  Future<String> getCSRF()
+   async {
+     var CSRF=await CallApi().getData('getSessionToken');
+     //print(CSRF);
+     SharedPreferences localStorage = await SharedPreferences.getInstance();
+     localStorage.setString('CSRF', CSRF);
+     return CSRF;
+  }
+}
