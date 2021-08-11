b59dbb2f0827937d1496edc3566e80abbe1e9d65
atyourdoorstep <atyourdoorstep.pk@gmail.com>
2021-08-11 05:54:08 +0500

    api url changed

--- lib/api.dart
+++ lib/api.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 class CallApi{
   //final String _url = 'http://127.0.0.1:8000/api';//for web
-  final String _url = 'http://10.0.2.2:8000/api';//for emulator
+  //final String _url = 'http://10.0.2.2:8000/api';//for emulator
+  final String _url = 'http://test-at-your-door-step.herokuapp.com/api';//for emulator
 
 
   postData(data, apiUrl) async {
